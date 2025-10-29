e82740406aed871934ef172a197ed2ecbc9962ce
Leonardo Tireck <leotireck@gmail.com>
2025-10-29 18:32:45 -0300

    change quicksort implementation to in-place

--- QuickSort.ts
+++ QuickSort.ts
@@ -1,21 +1,25 @@
-function quickSort(arr: Array<number>) {
-  if (arr.length <= 1) {
-    return arr;
+function quickSort(arr: number[], low = 0, high = arr.length - 1): number[] {
+  if (low < high) {
+    const pivotIndex = partition(arr, low, high);
+    quickSort(arr, low, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, high);
   }
+  return arr;
+}
 
-  const pivot = arr[arr.length - 1];
-  const left: number[] = [];
-  const right: number[] = [];
+function partition(arr: number[], low: number, high: number): number {
+  const pivot = arr[high];
+  let i = low - 1;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    console.log(arr, left, right);
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
+  for (let j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      [arr[i], arr[j]] = [arr[j], arr[i]]; // swap
     }
   }
-  return [...quickSort(left), pivot, ...quickSort(right)];
+
+  [arr[i + 1], arr[high]] = [arr[high], arr[i + 1]];
+  return i + 1;
 }
 
 const unsortedArray = [52, 5, 1, 3, 6, 7, 8, 54, 7, 8, 9, 9, 6, 6, 4, 3];
